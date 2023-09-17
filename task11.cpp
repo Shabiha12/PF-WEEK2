9a906f0f8fb27e8b26e63199a81383257f18febd
Shabiha12 <144408567+Shabiha12@users.noreply.github.com>
2023-09-16 21:48:49 -0700

    Add files via upload

---
+++ task11.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+main(){
+cout<<"            +--^---------------,----------,-------,-------,-----^-         "<<endl;
+cout<<"              |   ||||||           |---------------|           |                   o      "<<endl;
+cout<<"             .--------------------------------------^----------------|      "<<endl;
+cout<<"                    ---------,----------,---------,-----------------,          "<<endl;
+cout<<"                      //xxxxxxxxxxx //,                    //                    "<<endl;
+cout<<"                     //xxxxxxxxxxx/                                               "<<endl;            
+cout<<"                     //xxxxxxxxxxx/                                                "<<endl;
+cout<<"                     // xxxxxxxxxx/                                                  "<<endl;
+cout<<"                     //xxxxxxxxxx/                                                   "<<endl;
+cout<<"                     ( --------------(                                                     "<<endl;
+cout<<"                       ,------------,                                                       "<<endl;
+}
---
+++ task7.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+main(){
+cout<<"  -----------------------------------  "<<endl;
+cout<<"            .  ^_^                            "<<endl;
+cout<<"             .(oo) \\________        "<<endl;
+cout<<"              ( _ )\                )\\         "<<endl;
+cout<<"                       | | - - - w       |      "<<endl;
+cout<<"                       | |                | |      "<<endl;
+}
